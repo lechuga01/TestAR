0105647f614758c8918f6fd5e89cef18a383dfb3
shadow <lechu@MacBook-Air-de-shadow.local>
2016-12-07 16:45:29 -0600

    se modifico el final de linea para poder usarse

--- ItemBiblioteca.php
+++ ItemBiblioteca.php
@@ -12,17 +12,18 @@
 		die("Conesion muerta".mysqli_connect_error());
 
 	}
-	$sql = "Select NoSistema from biblioteca";
+	$sql = "Select * from biblioteca";
 	
 	$result = mysqli_query($conn,$sql);
 
-	echo "antes del if";
+	//echo "antes del if";
 
 	if(mysqli_num_rows($result) > 0){
 		//echo "es 0";
-		//muestra la informacion
+		//muestra la informacion 
 		while($row = mysqli_fetch_assoc($result)){
-			echo "NoSistema:" . $row['NoSistema'] . ";";
+			echo "|NoSistema " . $row['NoSistema'] . "|ISBN " . $row['ISBN']. "|Autor " . $row['Autor'] . "|TUniforme " . $row['TUniforme'] . "|Titulo " . $row['Titulo'] . "|Edicion " . $row['Edicion'] . "|PieDeImprenta " .$row['PieDeImprenta'] . "|DescrFisica " . $row['DescrFisica'] . 
+			"|Nota " . $row['Nota'] . "|TemaGeneral " . $row['TemaGeneral'] . "|ASecPersonas " . $row['ASecPersonas'] . "|BaseLogica " . $row['BaseLogica'] . "*";
 		}
 	}
 
